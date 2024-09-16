963c0f07dfd566de884867c7611a9f942d4072f0
aaksant <aaksangit@gmail.com>
2024-09-16 14:32:20 +0700

    Initial commit

---
+++ src/modules/Ship.js
@@ -0,0 +1,20 @@
+export default class Ship {
+  constructor() {
+    // Largest ship has the length of 3
+    this.length = 3;
+    this.hitCount = 0;
+    this.sink = false;
+  }
+
+  hit() {
+    this.hitCount++;
+  }
+
+  isSunk() {
+    if (this.hitCount === this.length) {
+      this.sink = true;
+      return this.sink;
+    }
+    return this.sink;
+  }
+}
