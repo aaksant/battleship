44c6ece310145a2470688bdcce375f3c7acbeb7c
aaksant <aaksangit@gmail.com>
2024-09-16 23:32:47 +0700

    build: update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,22 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 2021,
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
   pluginJs.configs.recommended,
   {
     rules: {
-      'no-unused-vars': 'off'
-    },
-    parserOptions: {
-      sourceType: 'module'
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn'
     }
   }
 ];
