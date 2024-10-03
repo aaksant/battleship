df94a96452332df5d301eafc556cd4c20ae5733f
aaksant <aaksangit@gmail.com>
2024-10-03 20:19:03 +0700

    feat: load initial game user interface

--- src/view/Game.js
+++ src/view/Game.js
@@ -6,5 +6,29 @@ import Handlers from './Handlers';
 export default class Game {
   constructor() {
     this.handlers = new Handlers();
+    this.playerBoard = new Board();
+    this.opponentBoard = new Board();
+    this.initPlayersBoard();
+  }
+
+  initPlayersBoard() {
+    this.createBoard(
+      this.playerBoard.grid,
+      document.querySelector('.player-board')
+    );
+    this.createBoard(
+      this.opponentBoard.grid,
+      document.querySelector('.opponent-board')
+    );
+  }
+
+  createBoard(grid, boardElement) {
+    for (let row = 0; row < grid.length; row++) {
+      for (let col = 0; col < grid[row].length; col++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        boardElement.appendChild(cell);
+      }
+    }
   }
 }
