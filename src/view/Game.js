e096bb6d1be6e82f2eed9f84c0e959830c0a03f7
aaksant <aaksangit@gmail.com>
2024-10-25 23:32:32 +0700

    refactor: move boards creation to `Setup.js`

--- src/view/Game.js
+++ src/view/Game.js
@@ -6,33 +6,6 @@ import Setup from './Setup';
 export default class Game {
   constructor() {
     this.setup = new Setup();
-    this.playerBoard = new Board();
-    this.opponentBoard = new Board();
-    this.initPlayersBoard();
-  }
-
-  initPlayersBoard() {
-    this.createBoard(
-      this.playerBoard.grid,
-      document.querySelector('.player-board')
-    );
-    this.createBoard(
-      this.opponentBoard.grid,
-      document.querySelector('.opponent-board')
-    );
-  }
-
-  createBoard(grid, boardElement) {
-    for (let row = 0; row < grid.length; row++) {
-      for (let col = 0; col < grid[row].length; col++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.classList.add('dropzone');
-        cell.dataset.row = row;
-        cell.dataset.col = col;
-
-        boardElement.appendChild(cell);
-      }
-    }
+    this.setup.initBoards();
   }
 }
