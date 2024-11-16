15dc283b789d801846fad96137c2e70a02c8189e
aaksant <aaksangit@gmail.com>
2024-11-16 10:25:23 +0700

    refactor: change `x`, `y` to `row`, `col`

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -4,25 +4,25 @@ export default class Player {
     this.attackedCoords = new Set();
   }
 
-  isAlreadyAttacked(x, y) {
-    return this.attackedCoords.has(`${x}, ${y}`);
+  isAlreadyAttacked(row, col) {
+    return this.attackedCoords.has(`${row}, ${col}`);
   }
 
-  attack(x, y, board) {
-    if (this.isAlreadyAttacked(x, y)) return false;
+  attack(row, col, board) {
+    if (this.isAlreadyAttacked(row, col)) return false;
 
-    this.attackedCoords.add(`${x}, ${y}`);
-    return board.receiveAttack(x, y);
+    this.attackedCoords.add(`${row}, ${col}`);
+    return board.receiveAttack(row, col);
   }
 
   randomAttack(board) {
-    let x, y;
+    let row, col;
     // Set x and y first
     do {
-      x = Math.floor(Math.random() * board.size);
-      y = Math.floor(Math.random() * board.size);
-    } while (this.isAlreadyAttacked(x, y));
+      row = Math.floor(Math.random() * board.size);
+      col = Math.floor(Math.random() * board.size);
+    } while (this.isAlreadyAttacked(row, col));
 
-    return this.attack(x, y, board);
+    return this.attack(row, col, board);
   }
 }
